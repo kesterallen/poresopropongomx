fb5a3c50227e2f79f7f5425d852134d2436c17cc
Kester Allen <kester@gmail.com>
2014-12-14 18:53:55 -0800

    Adding an additional copy of just the image list to the crontab

--- imagemake.bash
+++ imagemake.bash
@@ -1,2 +1,3 @@
-lftp ftp.fatcow.com -u 'poresopropongomx,Newyork1234!' -e 'cd images && find . && exit' >| '/media/sf_dropbox/Mosaic PNGs/image_list.txt'
-lftp ftp.fatcow.com -u 'poresopropongomx,Newyork1234!' -e 'mirror --verbose=3 --reverse --delete /home/kester/Dropbox/Mosaic\ PNGs/ /images && exit'
+7  */6   *   *   *     lftp ftp.fatcow.com -u 'poresopropongomx,Newyork1234!' -e 'mirror --verbose=3 --reverse --delete /home/kester/Dropbox/Mosaic\ PNGs/ /images && exit'
+30 */6   *   *   *     lftp ftp.fatcow.com -u 'poresopropongomx,Newyork1234!' -e 'cd images && find . && exit' >| '/home/kester/Dropbox/Mosaic PNGs/image_list.txt'
+35 */6   *   *   *     cd /home/kester/Dropbox/Mosaic\ PNGs && lftp ftp.fatcow.com -u 'poresopropongomx,Newyork1234!' -e 'cd images && put image_list.txt && exit'
