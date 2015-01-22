1749430a2d09296714db6b3b3b73a26ae92ee886
Kester Allen <kester@gmail.com>
2015-01-22 13:15:02 -0800

    Remove counts from social media buttons. Replace link on logo. Move facebook button setup into navbar.html

--- cgi-bin/renderer.py
+++ cgi-bin/renderer.py
@@ -56,18 +56,6 @@ PAGE_TEMPLATE = """
   <title>Por Eso Propongo</title>
 </head>
 
-<body role="document">
-<!-- Hook for FB Share button-->
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<!-- Hook for FB Share button end-->
-
 <div class="container-fluid">
   %s <!-- navbar -->
   <div class="row">
